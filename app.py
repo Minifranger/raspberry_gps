3d0b71030ca72f4d8c8a323b106acacecff0506e
Minifranger <clement.franger@gmail.com>
2018-10-13 18:35:00 +0200

    cast float

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ gps_socket.watch()
 for new_data in gps_socket:
     if new_data:
         data_stream.unpack(new_data)
-        print("Latitude = {lat}".format(lat=round(data_stream.TPV['lat'], 8)), 
-              "Longitude = {lon}".format(lon=round(data_stream.TPV['lon'], 8)))
+        print("Latitude = {lat}".format(lat=round(float(data_stream.TPV['lat']), 8)),
+              "Longitude = {lon}".format(lon=round(float(data_stream.TPV['lon']), 8)))
 
