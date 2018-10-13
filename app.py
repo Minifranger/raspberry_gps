9cd7f9858de7ffaa70c811d85b0471c8a11c9009
Minifranger <clement.franger@gmail.com>
2018-10-13 18:39:07 +0200

    adding logging

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from gps3 import gps3
-
+import loggers
 
 gps_socket = gps3.GPSDSocket()
 data_stream = gps3.DataStream()
@@ -8,6 +8,8 @@ gps_socket.watch()
 for new_data in gps_socket:
     if new_data:
         data_stream.unpack(new_data)
-        print("Latitude = {lat}".format(lat=data_stream.TPV['lat']),
+        # print("Latitude = {lat}".format(lat=data_stream.TPV['lat']),
+        #       "Longitude = {lon}".format(lon=data_stream.TPV['lon']))
+        loggers.gps_data.info("Latitude = {lat}".format(lat=data_stream.TPV['lat']),
               "Longitude = {lon}".format(lon=data_stream.TPV['lon']))
 
