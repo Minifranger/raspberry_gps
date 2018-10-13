05fd8a5151cfac1ef7626b6d282ca722165eaf2c
Minifranger <clement.franger@gmail.com>
2018-10-13 18:21:59 +0200

    test print

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
+import gps3
 
-app = Flask(__name__)
+gps_socket = gps3.GPSDSocket()
+data_stream = gps3.DataStream()
+gps_socket.connect()
+gps_socket.watch()
+for new_data in gps_socket:
+    if new_data:
+        data_stream.unpack(new_data)
+        print("Longitude = {lon}".format(lon=data_stream.TPV['lon']), "Latitude = {lat}".format(lat=data_stream.TPV['lat']))
 
-
-@app.route('/')
-def index():
-    return 'Hello world'
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
---
+++ loggers.py
@@ -0,0 +1,14 @@
+import logging.handlers
+
+MODE = "a"
+MAX_BYTES = 2 * 1024 * 1024
+BACKUP = 1
+
+formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
+
+handler = logging.handlers.RotatingFileHandler("logs/gps_data.log", mode=MODE, maxBytes=MAX_BYTES, backupCount=BACKUP)
+handler.setFormatter(formatter)
+
+gps_data = logging.getLogger("gps_data")
+gps_data.setLevel(logging.INFO)
+gps_data.addHandler(handler)
