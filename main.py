2c189c069c87d84cefaf9f88ced8f52eab1f47f4
Minifranger <clement.franger@gmail.com>
2018-10-15 21:45:38 +0200

    test cast

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import properties
 conn = sqlite3.connect(properties.GPS_DB)
 c = conn.cursor()
 
-c.execute(""" CREATE TABLE if not exists gps_data (date text, lat real, lon real) """)
+c.execute(""" CREATE TABLE if not exists gps_data (date text, lat float, lon float) """)
 
 gps_socket = gps3.GPSDSocket()
 data_stream = gps3.DataStream()
