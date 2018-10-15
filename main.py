45f5769081eb857dc6abab83f79212367137bb6a
Minifranger <clement.franger@gmail.com>
2018-10-15 21:49:39 +0200

    test

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ for new_data in gps_socket:
     if new_data:
         data_stream.unpack(new_data)
         if type(data_stream.TPV["lat"]) is float and type(data_stream.TPV["lon"]) is float:
-            c.execute("INSERT INTO gps_data VALUES ({date}, {lat}, {lon})".format(date=str(data_stream.TPV["time"]),
-                                                                                  lat="%.8f" % (data_stream.TPV["lat"]),
-                                                                                  lon="%.8f" % (data_stream.TPV["lon"])))
+            c.execute(""" INSERT INTO gps_data(date, lat, lon) VALUES (?, ?, ?); """, (data_stream.TPV["time"],
+                                                                                       "%.8f" % (data_stream.TPV["lat"]),
+                                                                                       "%.8f" % (data_stream.TPV["lon"])))
             conn.commit()
             loggers.gps_data.info("Latitude = {lat}, Longitude = {lon}".format(lat="%.8f" % (data_stream.TPV["lat"]),
                                                                                lon="%.8f" % (data_stream.TPV["lon"])))
