efb522a16b9e3aac89a3b1c7e0d3cb1ca44c0238
Minifranger <clement.franger@gmail.com>
2018-10-23 21:13:03 +0200

    fix time

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ gps_socket, data_stream = gps.start_stream()
 for new_data in gps_socket:
     if new_data:
         data_stream.unpack(new_data)
-        time = datetime.strptime(data_stream.TPV["time"], '%Y-%m-%dT%H:%M:%S.%fZ')
-        lat, lon = data_stream.TPV["lat"], data_stream.TPV["lon"]
+        time, lat, lon = data_stream.TPV["time"], data_stream.TPV["lat"], data_stream.TPV["lon"]
         if gps.check_data(lat=lat, lon=lon):
             c.execute(""" INSERT INTO gps_data(date, lat, lon) VALUES (?, ?, ?); """, (time, lat, lon))
             conn.commit()
