cd4d6907487ec4f3a4aa1a6168f9ec07ab73b01d
Minifranger <clement.franger@gmail.com>
2018-10-23 21:01:04 +0200

    fix datetime

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import gps
 import loggers
 import db
 import properties
+from datetime import datetime
 
 conn, c = db.start_db(name=properties.GPS_DB)
 
@@ -10,7 +11,8 @@ gps_socket, data_stream = gps.start_stream()
 for new_data in gps_socket:
     if new_data:
         data_stream.unpack(new_data)
-        time, lat, lon = data_stream.TPV["time"], data_stream.TPV["lat"], data_stream.TPV["lon"]
+        time = datetime.strptime(data_stream.TPV["time"], '%Y-%m-%dT%H:%M:%S.%fZ')
+        lat, lon = data_stream.TPV["lat"], data_stream.TPV["lon"]
         if gps.check_data(lat=lat, lon=lon):
             c.execute(""" INSERT INTO gps_data(date, lat, lon) VALUES (?, ?, ?); """, (time, lat, lon))
             conn.commit()
