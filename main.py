5e226d028f5b718e01c90aa92dc766aaa4aea540
Minifranger <clement.franger@gmail.com>
2019-09-26 20:32:48 +0200

    fix stream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3.5
-import sensor
 import loggers
 from gps3 import gps3
 import properties
@@ -15,17 +14,10 @@ print(gps_db.list_tables())
 
 gps_sensor = sensor.Sensor(socket=gps3.GPSDSocket(), stream=gps3.DataStream())
 gps_sensor.connect()
-# conn, c = db.start_db(name=properties.GPS_DB)
 
-# gps_socket, data_stream = gps.start_stream()
 for new_data in gps_sensor.socket:
     if new_data:
         gps_sensor.stream.unpack(new_data)
-        print(new_data)
-        sio.emit("new_data", new_data)
-#         time, lat, lon = data_stream.TPV["time"], data_stream.TPV["lat"], data_stream.TPV["lon"]
-#         if gps.check_data(lat=lat, lon=lon):
-#             c.execute(""" INSERT INTO gps_data(date, lat, lon) VALUES (?, ?, ?); """, (time, lat, lon))
-#             conn.commit()
-#             loggers.gps_data.info("Latitude = {lat}, Longitude = {lon}".format(lat=lat, lon=lon))
-# conn.close()
+        time, lat, lon = gps_sensor.stream.TPV["time"], gps_sensor.stream.TPV["lat"], gps_sensor.stream.TPV["lon"]
+        print("lat = {lat} - lon = {lon}".format(lat=lat, lon=lon))
+        sio.emit("new_data", gps_sensor.stream.TPV)
