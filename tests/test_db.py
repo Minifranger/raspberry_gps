f63e393b2dc4e996dd1fb16e46164b82dbaf51c3
Minifranger <clement.franger@gmail.com>
2019-10-17 23:56:35 +0200

    finish db tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -12,19 +12,24 @@ class TestDB(unittest.TestCase):
         test_db = db.MetaDatabase(path=self.db_path)
         self.assertTrue(isfile(test_db.path))
 
+    def test_del(self):
+        test_db = db.MetaDatabase(path=self.db_path)
+        test_db.__exit__(None, None, None)
+        test_db._del()
+        self.assertFalse(isfile(self.db_path))
+
     def test_create_table(self):
         test_db = db.MetaDatabase(path=self.db_path)
         test_table = db.MetaTable(name=self.table_name, schema=self.table_schema)
-        test_table2 = db.MetaTable(name="test2", schema=self.table_schema)
         test_db.create_table(test_table)
-        test_db.create_table(test_table2)
-        print(test_db.list_tables())
-        print(type(test_db.list_tables()[0]))
-        self.assertTrue(self.table_name in [table for (table, _) in test_db.list_tables()])
+        self.assertTrue(self.table_name in [table for (table, ) in test_db.list_tables()])
 
     def test_delete_table(self):
         test_db = db.MetaDatabase(path=self.db_path)
-        self.assertTrue(isfile(test_db.path))
+        test_table = db.MetaTable(name=self.table_name, schema=self.table_schema)
+        test_db.create_table(test_table)
+        test_db.delete_table(test_table)
+        self.assertFalse(self.table_name in [table for (table, ) in test_db.list_tables()])
 
 
 if __name__ == '__main__':
